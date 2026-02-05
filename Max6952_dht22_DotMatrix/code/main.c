c62a03b0df2294deee8a817b0e31f791904a58fd
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-02-05 11:28:11 +0200

    Add files via upload

---
+++ Max6952_dht22_DotMatrix/code/main.c
@@ -0,0 +1,45 @@
+#include<reg52.h>
+#include <./assets/delay/delay.h>
+#include <./assets/dht22/dht22.h>
+#include <./assets/Max6952/Max6952.h>
+//------------------------------------------------------------------------------
+char str[15];
+unsigned char sensorData[5];
+int hum, tempr;
+//------------------------------------------------------------------------------
+int main() 
+{	  
+  MAX6952_init(3);    
+  dht22_init();
+
+  while(1)
+  { 
+		oneWire_start();
+		
+		if(oneWire_getResponse())
+		{	
+			dht22_getSensorData(sensorData);
+		}
+		
+		if(dht22_isChecksumInvalid(sensorData))
+		{
+			MAX6952_displayText(" Err");
+		}
+		else
+				{
+					hum = dht22_getHumidity(sensorData);
+					tempr = dht22_getTemperature(sensorData);
+				
+					if(modeNum == 0 || modeNum == 1)
+							sprintf(str, "%6.1f°%c", calcTempr(tempr), (modeNum == 0) ? 'C' : 'F');
+					else
+							if(modeNum == 2)
+									sprintf(str, "%7.1f%c", (float) hum/10, '%');
+						
+					MAX6952_displayText(str);
+				}
+		
+		__delay_ms(100);
+  }
+}
+//------------------------------------------------------------------------------
