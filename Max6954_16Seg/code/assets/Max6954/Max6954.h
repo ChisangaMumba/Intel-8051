9e6fd033e4535e2a57412e8912010fd2bfaf678c
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-02-05 11:35:45 +0200

    Add files via upload

---
+++ Max6954_16Seg/code/assets/Max6954/Max6954.h
@@ -0,0 +1,66 @@
+#ifndef MAX6954_H
+#define	MAX6954_H
+//------------------------Dot Matrix LED  Driver--------------------------------
+#include "MAX6954_Prototypes.h"
+//------------------------------------------------------------------------------
+void MAX6954_init()
+{
+  SPI_init();
+	
+  MAX6954_write(DECODE_MODE_REG, DECODE_ALL);         //Decode ascii characters
+  MAX6954_write(GLOBAL_INTESITY_REG, BRIGHTNESS); 
+  MAX6954_write(SCAN_LIMIT_REG, SCAN_ALL_DIGITS);
+  MAX6954_write(SHUTDOWN_REG, NORMAL_OPERATION);
+}
+//------------------------------------------------------------------------------
+void MAX6954_write(char reg, signed char data_)
+{
+  CS = 0; 
+  
+  SPI_write(reg); 
+  SPI_write(data_); 
+		
+  CS = 1;
+}
+//------------------------------------------------------------------------------
+void MAX6954_displayNumber(long number)
+{   
+	char strNum[15]; 
+  sprintf(strNum, "%6li", number);
+  
+	MAX6954_displayText(strNum);
+}
+//------------------------------------------------------------------------------
+void MAX6954_displayText(char text[])
+{
+	char i, chr;
+						
+  for(i = 0; i < strlen(text); i++)
+  {  
+		chr = text[i];			
+		
+		if(chr & 0x80)									//All non-standard ascii chars are above 128
+			chr = getCustomChar(chr); 
+		
+		MAX6954_write( DISPLAY_REG0 + i, chr);
+  }
+}
+//------------------------------------------------------------------------------
+char getCustomChar(char asciiChar)
+{
+	char i = 0, digit = 0x00;
+  
+	while(customChar[i])
+	{
+		 if(asciiChar == customChar[i])
+		 {
+		   digit = charCode[i]; 
+			 break;
+		 }
+		 i++;
+	}	
+					
+	return digit;			
+}
+//------------------------------------------------------------------------------
+#endif	
---
+++ Max6954_16Seg/code/assets/dht22/dht22.h
@@ -0,0 +1,72 @@
+#ifndef DHT22_H
+#define	DHT22_H
+//------------------------------------------------------------------------------
+#include <assets/oneWire/oneWire.h>
+#include <assets/Ext_Intr/Ext_Intr.h>
+#include <assets/dht22/dht22_prototypes.h>
+//------------------------------------------------------------------------------
+void dht22_init()
+{
+	ExtInt0_init();
+}
+//------------------------------------------------------------------------------
+unsigned char dht22_calcChecksum(unsigned char sensorData[])
+{
+	unsigned char i, checksum = 0;
+	
+	for(i = 0; i < 4; i++)
+	{
+		checksum += sensorData[i];
+	}
+	
+	return checksum;
+}
+//------------------------------------------------------------------------------
+unsigned char dht22_isChecksumInvalid(unsigned char sensorData[])
+{
+	unsigned char isInvalid = 0;
+	
+	if(dht22_calcChecksum(sensorData) != sensorData[4]) // sensorData[4] == checksum value
+	{
+		isInvalid = 1;
+	}
+	
+	return isInvalid;
+}
+//------------------------------------------------------------------------------
+signed int dht22_getTemperature(unsigned char sensorData[])
+{
+  signed int tempr = (sensorData[2] << 8) + sensorData[3];
+    
+  return tempr;
+}
+//------------------------------------------------------------------------------
+int dht22_getHumidity(unsigned char sensorData[])
+{
+  int humidity = (sensorData[0] << 8) + sensorData[1];
+    
+  return humidity;
+}
+//------------------------------------------------------------------------------
+void dht22_getSensorData(unsigned char sensorData[])
+{
+	unsigned char i;
+	for(i = 0; i < 5; i++)
+	{
+		sensorData[i] = oneWire_readByte();
+	}
+}
+//------------------------------------------------------------------------------
+float calcTempr(signed int rawData)
+{
+	float tempr = rawData ;  
+   	
+	if(rawData & 0x8000) //if 16th _bit is a 1, num is negative (sign magnitude signed number)
+	{
+	  tempr = -1 * (rawData & 0x7fff);
+	}
+
+	return (modeNum == 0) ? tempr/10 : ((float) (tempr/10) * 9/5) + 32; // C or F
+}
+//------------------------------------------------------------------------------
+#endif
